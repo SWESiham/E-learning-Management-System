ebc24863f2bd4cac571b3ffd1f9a2c62efeafab1
monasaad1512 <monasaad221049@gmail.com>
2025-12-01 11:57:46 +0200

    create home page without routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,18 @@ import { Component, signal } from '@angular/core';
   standalone: false,
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('LMS');
+
+  showNavbar: boolean = true;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.showNavbar = event.urlAfterRedirects !== '/';
+    });
+  }
 }
